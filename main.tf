e013e86f968a54168c8a4eea604372e7222883e6
Damon Green <damon.green@btinternet.com>
2021-02-18 18:26:57 +0000

    update AGrp ID

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ output "webtests_synthetic_id" {
 resource "azurerm_monitor_action_rule_action_group" "example" {
   name                = "coded-rule"
   resource_group_name = azurerm_resource_group.rg.name
-  action_group_id     = azurerm_monitor_action_group.appinsights.id
+  action_group_id     = azurerm_monitor_action_group.Ndiv44.id
   enabled                 = true
 
 
