bcc9532b94074a32522a0bea93b0f5ef5a157e9f
Damon Green <damon.green@btinternet.com>
2021-02-24 18:01:24 +0000

    add a metric alert for webtest

--- main.tf
+++ main.tf
@@ -135,7 +135,7 @@ resource "azurerm_application_insights_web_test" "appinsights-2" {
 resource "azurerm_monitor_metric_alert" "appinsights" {
   name                = "nfdiv-metricalert1"
   resource_group_name = azurerm_resource_group.rg.name
-  scopes              = azurerm_application_insights_web_test
+  scopes              = azurerm_application_insights_web_test.appinsights
   description         = "Action will be triggered when count is less than 90%"
 
   application_insights_web_test_location_availability_criteria {
